f53a784fdcb65fb2ad87d7eea5254482c1ad92f3
Felixyang777 <1761610526@qq.com>
2023-10-04 19:11:00 +0800

    code update

--- 代码-Code/舵机/SG-90/Servos/Servos.ino
+++ 代码-Code/舵机/SG-90/Servos/Servos.ino
@@ -1,20 +1,20 @@
 #include <Servo.h>            //加载文件库
 int pos = 0;
 Servo myservo;
- 
+
 void setup()
 {
-  myservo.attach(9, 500, 2500);          //修正脉冲宽度
+	myservo.attach(10);          //修正脉冲宽度
 }
- 
+
 void loop()
 {
-  for (pos = 0; pos <= 180; pos += 1) {       //pos+=1等价于pos=pos+1
-    myservo.write(pos);
-    delay(15);					
-  }
-  for (pos = 180; pos >= 0; pos -= 1) {
-    myservo.write(pos);
-    delay(15); 					
-  }
+	for (pos = 0; pos <= 90; pos += 1) {       //pos+=1等价于pos=pos+1
+		myservo.write(pos);
+	}
+	delay(1000);
+	for (pos = 90; pos >= 0; pos -= 1) {
+		myservo.write(pos);
+	}
+	delay(1000);
 }
