8ab1b6ef2da0dca917e23f2cb514b7167f5fa3fb
yang felix <1761610526@qq.com>
2023-10-19 19:19:42 +0800

    Jewel-Rose-Pink-Ink-Gold

--- 代码-Code/Jewel-Rose-Pink-Ink-Gold/Jewel-Rose-Pink-Ink-Gold/Jewel-Rose-Pink-Ink-Gold.ino
+++ 代码-Code/Jewel-Rose-Pink-Ink-Gold/Jewel-Rose-Pink-Ink-Gold/Jewel-Rose-Pink-Ink-Gold.ino
@@ -1,9 +1,15 @@
+  //////////// 
+ /*TCRT5000*/
+////////////
+int pin = 7;      //定义数字检测接口D2
+int val;          //定义变量
+
   ///////////////
  /*fingerprint*/
 ///////////////
-#include <Adafruit_Fingerprint.h>
+/*#include <Adafruit_Fingerprint.h>
 SoftwareSerial mySerial(2, 3);  // RX,TX(UNO)
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);*/
 uint8_t id;
 
 
@@ -11,129 +17,39 @@ uint8_t id;
  /*servo*/
 /////////
 #include <Servo.h>  
-int pos = 0;
-Servo myservo;
 
+Servo myservo;// 创建舵机对象来控制舵机
+ // 大多数板子上可以创建12个舵机对象
+ int pos = 0;    // pos变量用于存储舵机位置
+ int speed = 6; // speed变量用于控制舵机转动的速度，单位ms
+ // Arduino UNO中,pwm引脚是3、5、6、9、10和11
 
 void setup() {
-     myservo.attach(10); 
-
-    Serial.begin(9600);
-    while (!Serial) ; 
-    delay(100);
-    Serial.println("\n\nAdafruit Fingerprint sensor enrollment");
-
-    // set the data rate for the sensor serial port
-    finger.begin(57600);
-
-    if (finger.verifyPassword()) {
-        Serial.println("Found fingerprint sensor! :) ");
-    }
-    else {
-        Serial.println("Did not find fingerprint sensor  :( ");
-        while (1) { delay(1); }
-    }
+  myservo.attach(10);    // 将舵机连接到9号引脚的舵机对象
+  pinMode(pin, INPUT);  // 红外寻迹连接引脚D2，并设置为输入模式
+  Serial.begin(9600);  //设置串门波特率为9600kbps
 }
+void loop() {
+  myservo.attach(10);
+  val = digitalRead(pin);  //读取数字接口的值
+  Serial.println(val);     //输出输出接门的值
+  if (val == 1&&pos < 45)            //如果pin2检测的值为高电平，点亮LED
+  {
+ 
+      for (pos = 0; pos <= 90; pos += 1)  // 从0度逐渐转动到180度 每次正方向转动1度
+      {
+        myservo.write(pos);  // 告诉舵机转到变量'pos'所表示的位置
+        delay(speed);        // 等待15毫秒，让舵机到达目标位置
+      }
+    
 
-uint8_t readnumber(void) {
-    uint8_t num = 0;
+  } else if(val != 1&&pos > 45) {  //如果为低电平熄灭LED
 
-    while (num == 0) {
-        while (!Serial.available())
-            ;
-        num = Serial.parseInt();
-    }
-    return num;
-}
-
-void loop()  // run over and over again
-{
-    getFingerprintID();
-    delay(50);  //don't ned to run this at full speed.
-    InsertionDetection();
-    delay(50);  //don't ned to run this at full speed.
-}
-
-uint8_t getFingerprintID() {
-    uint8_t p = finger.getImage();
-    switch (p) {
-    case FINGERPRINT_OK:
-        Serial.println("Image taken");
-        break;
-    case FINGERPRINT_NOFINGER:
-        Serial.println("No finger detected");
-        return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-        Serial.println("Communication error");
-        return p;
-    case FINGERPRINT_IMAGEFAIL:
-        Serial.println("Imaging error");
-        return p;
-    default:
-        Serial.println("Unknown error");
-        return p;
-    }
-
-    // OK success!
-
-    p = finger.image2Tz();
-    switch (p) {
-    case FINGERPRINT_OK:
-        Serial.println("Image converted");
-        break;
-    case FINGERPRINT_IMAGEMESS:
-        Serial.println("Image too messy");
-        return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-        Serial.println("Communication error");
-        return p;
-    case FINGERPRINT_FEATUREFAIL:
-        Serial.println("Could not find fingerprint features");
-        return p;
-    case FINGERPRINT_INVALIDIMAGE:
-        Serial.println("Could not find fingerprint features");
-        return p;
-    default:
-        Serial.println("Unknown error");
-        return p;
-    }
-
-    // OK converted!
-    p = finger.fingerFastSearch();
-    if (p == FINGERPRINT_OK) {
-        Serial.println("Found a print match!");
-    }
-    else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-        Serial.println("Communication error");
-        return p;
-    }
-    else if (p == FINGERPRINT_NOTFOUND) {
-        Serial.println("Did not find a match");
-        return p;
-    }
-    else {
-        Serial.println("Unknown error");
-        return p;
-    }
-
-    // found a match!
-    Serial.print("Found ID #");
-    Serial.print(finger.fingerID);
-    Serial.print(" with confidence of ");
-    Serial.println(finger.confidence);
-
-    if(pos == 0)
-    {
-        for (pos = 0; pos <= 90; pos += 1)
-        {
-		    myservo.write(pos);
-	    }
-	    delay(1000);
-    }
-    return finger.fingerID;
-}
-
-
-void InsertionDetection()
-{
+      for (pos = 90; pos >= 0; pos -= 1)  // 从0度逐渐转动到180度 每次负方向转动1度
+      {
+        myservo.write(pos);
+        delay(speed);
+      }
+    
+  }
 }
