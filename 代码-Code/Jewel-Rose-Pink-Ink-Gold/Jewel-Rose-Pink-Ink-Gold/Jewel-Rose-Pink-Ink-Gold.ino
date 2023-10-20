65d810486f3e458f9ac67b9374e0647fa7a355a9
yang felix <1761610526@qq.com>
2023-10-20 14:04:55 +0800

    Jewel-Rose-Pink-Ink-Gold update

--- 代码-Code/Jewel-Rose-Pink-Ink-Gold/Jewel-Rose-Pink-Ink-Gold/Jewel-Rose-Pink-Ink-Gold.ino
+++ 代码-Code/Jewel-Rose-Pink-Ink-Gold/Jewel-Rose-Pink-Ink-Gold/Jewel-Rose-Pink-Ink-Gold.ino
@@ -7,9 +7,9 @@ int val;          //定义变量
   ///////////////
  /*fingerprint*/
 ///////////////
-/*#include <Adafruit_Fingerprint.h>
+#include <Adafruit_Fingerprint.h>
 SoftwareSerial mySerial(2, 3);  // RX,TX(UNO)
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);*/
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 uint8_t id;
 
 
@@ -28,22 +28,33 @@ void setup() {
   myservo.attach(10);    // 将舵机连接到9号引脚的舵机对象
   pinMode(pin, INPUT);  // 红外寻迹连接引脚D2，并设置为输入模式
   Serial.begin(9600);  //设置串门波特率为9600kbps
+  while (!Serial)
+    ;  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+
+  Serial.println("\n\nAdafruit Fingerprint sensor enrollment");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
 }
+
+
+
+
 void loop() {
   myservo.attach(10);
   val = digitalRead(pin);  //读取数字接口的值
-  Serial.println(val);     //输出输出接门的值
-  if (val == 1&&pos < 45)            //如果pin2检测的值为高电平，点亮LED
-  {
+ // Serial.println(val);     //输出输出接门的值
+  getFingerprintIDez();
  
-      for (pos = 0; pos <= 90; pos += 1)  // 从0度逐渐转动到180度 每次正方向转动1度
-      {
-        myservo.write(pos);  // 告诉舵机转到变量'pos'所表示的位置
-        delay(speed);        // 等待15毫秒，让舵机到达目标位置
-      }
-    
-
-  } else if(val != 1&&pos > 45) {  //如果为低电平熄灭LED
+ if(val != 1&&pos > 45) {  //如果为低电平熄灭LED
 
       for (pos = 90; pos >= 0; pos -= 1)  // 从0度逐渐转动到180度 每次负方向转动1度
       {
@@ -52,4 +63,32 @@ void loop() {
       }
     
   }
+}
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK) return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK) return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK) return -1;
+
+  // found a match!
+for (pos = 0; pos <= 90; pos += 1)  // 从0度逐渐转动到180度 每次正方向转动1度
+      {
+        myservo.write(pos);  // 告诉舵机转到变量'pos'所表示的位置
+        delay(speed);        // 等待15毫秒，让舵机到达目标位置
+      }
+  Serial.print("Found ID #");
+  Serial.print(finger.fingerID);
+  Serial.print(" with confidence of ");
+  Serial.println(finger.confidence);
+  while (val != 1)
+  {
+   delay(5); 
+     val = digitalRead(pin); 
+  }
+
+  return finger.fingerID;
 }
